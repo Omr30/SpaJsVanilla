9b722a7f6fb512f17ababe379fa4b71f3f557be1
mr <oscarmenacho23@gmail.com>
2020-08-21 15:44:37 -0500

    Create templates characters

---
+++ src/pages/Character.js
@@ -0,0 +1,33 @@
+const Character = () => {
+    const view = `
+        <div class="Character-inner">
+            <article class="Character-card">
+                <img src="imagen" alt="name">
+                <h2>Name</h2>
+            </article>
+            <article class="Character-card">
+                <h3>
+                    Episodes:
+                </h3>
+                <h3>
+                    Status:
+                </h3>
+                <h3>
+                    Specie:
+                </h3>
+                <h3>
+                    Gender:
+                </h3>
+                <h3>
+                    Origin:
+                </h3>
+                <h3>
+                    Last Location:
+                </h3>
+            </article>
+        </div>
+    `;
+    return view
+};
+
+export default Character
