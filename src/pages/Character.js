bf942e5d8d45b8f91342dcae7012522b16b9241f
mr <oscarmenacho23@gmail.com>
2020-08-21 17:29:38 -0500

    Agregado la iteracion de cada personaje muestra su informacion

--- src/pages/Character.js
+++ src/pages/Character.js
@@ -1,28 +1,33 @@
-const Character = () => {
+import getHash from '../utils/getHash'
+import getData from '../utils/getData'
+
+const Character = async () => {
+    const id = getHash()
+    const character = await getData(id)
     const view = `
         <div class="Character-inner">
             <article class="Character-card">
-                <img src="imagen" alt="name">
-                <h2>Name</h2>
+                <img src="${character.image}" alt="${character.name}">
+                <h2>${character.name}</h2>
             </article>
             <article class="Character-card">
                 <h3>
-                    Episodes:
+                    Episodes: <span>${character.episode.length}</span>
                 </h3>
                 <h3>
-                    Status:
+                    Status: <span>${character.status}</span>
                 </h3>
                 <h3>
-                    Specie:
+                    Specie: <span>${character.species}</span>
                 </h3>
                 <h3>
-                    Gender:
+                    Gender: <span>${character.gender}</span>
                 </h3>
                 <h3>
-                    Origin:
+                    Origin: <span>${character.origin.name}</span>
                 </h3>
                 <h3>
-                    Last Location:
+                    Last Location: <span>${character.location.name}</span>
                 </h3>
             </article>
         </div>
