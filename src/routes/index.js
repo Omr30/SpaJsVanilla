814d8e2137293e25e0912c6ed833e080cd4f48b3
mr <oscarmenacho23@gmail.com>
2020-08-21 16:59:10 -0500

    Add conection rout implement

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,6 +2,8 @@ import Header from '../templates/Header'
 import Home from '../pages/home'
 import Character from '../pages/Character'
 import Error404 from '../pages/Error404'
+import getHash from '../utils/getHash'
+import resolveRoutes from '../utils/resolveRoutes'
 
 const routes = {
     '/': Home,
@@ -14,6 +16,10 @@ const router = async () => {
     const content = null || document.getElementById('content');
 
     header.innerHTML = await Header()
+    let hash = getHash()
+    let route = await(resolveRoutes(hash))
+    let render = routes[route] ? routes[route] : Error404
+    content.innerHTML = await render()
 }
 
 export default router
